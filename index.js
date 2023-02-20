e4f67acd8629414be42667cf228b96d214feae3e
bergamotBen <bergamotBen@icloud.com>
2023-02-20 14:24:36 +0000

    mongoose connection created

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+const mongoString = process.env.DATABASE_URL;
 const app = express();
+mongoose.connect(mongoString);
+const database = mongoose.connection;
 
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-})
+  console.log(`Server Started at ${3000}`);
+});
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
