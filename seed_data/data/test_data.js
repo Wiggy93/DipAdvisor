37a5bc1b8b056414c1070af006996e0cf51d166c
Wiggy93 <110779323+Wiggy93@users.noreply.github.com>
2023-02-20 16:07:12 +0000

    seeding setup

---
+++ seed_data/data/test_data.js
@@ -0,0 +1,9 @@
+const locations = {
+  location_name: "Agden Resevoir",
+  created_by: "Mitchel",
+  image_url:
+    "https://fastly.picsum.photos/id/125/1500/1000.jpg?hmac=s6pniw6JFp6F753Ow9mnVAVqt8tOcP8ZlZE5-aJ22co",
+  description: "A water storage resevoir 6.5 miles west of Sheffield.",
+  public: true,
+};
+module.exports = locations;
---
+++ seed_data/seed.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Model = require("../model/model");
+const locations = require("./data/test_data");
+
+require("dotenv").config({
+  path: "./.env",
+});
+
+const mongoStr = process.env.DATABASE_URL;
+
+mongoose
+  .connect(mongoStr)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const seedDB = async () => {
+  try {
+    await Model.deleteMany({});
+    await Model.insertMany(locations);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+  console.log("disconnected from database");
+});
