50ee9bccc7533387dcb5fe574486ce50835384a7
lewqs <lewisbrown3108@gmail.com>
2023-02-22 17:01:00 +0000

    ok it needed the param

--- seed_data/data/test_data.js
+++ seed_data/data/test_data.js
@@ -2,7 +2,9 @@ const locations = [
   {
     location_name: "Agden Resevoir",
     created_by: "Mitchel",
-    image_urls: ["https://fastly.picsum.photos/id/125/1500/1000.jpg"],
+    image_urls: [
+      "https://fastly.picsum.photos/id/125/1500/1000.jpg?hmac=s6pniw6JFp6F753Ow9mnVAVqt8tOcP8ZlZE5-aJ22co",
+    ],
     description: "A water storage resevoir 6.5 miles west of Sheffield.",
     public: true,
   },
